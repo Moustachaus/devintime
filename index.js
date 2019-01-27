600241c07be82c2f25760627062370f51239e4c5
Moustachaus <moustachaus@gmail.com>
2019-01-26 23:56:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ const bot = new Discord.Client();
 
 bot.on('message', function(message){
     mention = message.mentions.users.first();
+    if (!msg.member.hasPermission("ADMINISTRATOR")) return;
     if (message.author.bot) return;
     if (message.startsWith = '!devintime msg')
     {
         if (mention == null) { return; }
         mentionMessage = message.content.slice (15);
         mention.sendMessage (mentionMessage);
-        message.channel.send ("done!");
+        message.channel.send ("Envoyé!");
     }
 });
 
