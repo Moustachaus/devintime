32794d2a7ac2cfec4885785994f8ff3eca800425
Moustachaus <moustachaus@gmail.com>
2019-01-26 23:09:53 -0500

    b

--- index.js
+++ index.js
@@ -32,80 +32,80 @@ bot.on('message', function(message){
 
 
 
-var Discord = require("discord.js");
 var prefix = "!dit";
-var client = new Discord.Client();
+
+
 
 var bannedwords = "fuck,shit,slut,whore".split(",");
 
-client.on("message", message => {
-  if (message.guild === null) return;
+bot.on("message", msg => {
+  if (msg.guild === null) return;
 
   for (i=0;i<bannedwords.length;i++) {
-    if (message.content.toLowerCase().includes(bannedwords[i])) {
-        message.delete();
-        message.reply("Please don't swear!");
+    if (msg.content.toLowerCase().includes(bannedwords[i])) {
+      msg.delete();
+      msg.reply("Please don't swear!");
       return;
     }
   }
 
-  if (message.author.bot) return;
-  if (!message.member.hasPermission("ADMINISTRATOR")) return;
+  if (msg.author.bot) return;
+  if (!msg.member.hasPermission("ADMINISTRATOR")) return;
 
-  if (!message.content.toLowerCase().startsWith(prefix)) return;
-  message.delete();
-  if (message.content.toLowerCase().startsWith(prefix + "kick ")) {
-    var mem = message.mentions.members.first();
+  if (!msg.content.toLowerCase().startsWith(prefix)) return;
+  msg.delete();
+  if (msg.content.toLowerCase().startsWith(prefix + "kick ")) {
+    var mem = msg.mentions.members.first();
     mem.kick().then(() => {
-        message.channel.send(mem.displayName + " has successfully been kicked by " + message.author.username + "!");
+      msg.channel.send(mem.displayName + " has successfully been kicked by " + msg.author.username + "!");
     }).catch(e => {
-        message.channel.send("An error occured!");
+      msg.channel.send("An error occured!");
     });
   }
-  if (message.content.toLowerCase().startsWith(prefix + "ban ")) {
-    var mem = message.mentions.members.first();
-    var mc = message.content.split(" ")[2];
+  if (msg.content.toLowerCase().startsWith(prefix + "ban ")) {
+    var mem = msg.mentions.members.first();
+    var mc = msg.content.split(" ")[2];
     mem.ban(mc).then(() => {
-        message.channel.send(mem.displayName + " has successfully been banned by " + message.author.username + " for " + mc + " days!");
+      msg.channel.send(mem.displayName + " has successfully been banned by " + msg.author.username + " for " + mc + " days!");
     }).catch(e => {
-        message.channel.send("An error occured!");
+      msg.channel.send("An error occured!");
     });
   }
-  if (message.content.toLowerCase().startsWith(prefix + "mute")) {
-    var mem = message.mentions.members.first();
-    if (message.guild.roles.find("name", "Muted")) {
-      mem.addRole(message.guild.roles.find("name", "Muted")).then(() => {
-        message.channel.send(mem.displayName + " has successfully been muted!");
+  if (msg.content.toLowerCase().startsWith(prefix + "mute")) {
+    var mem = msg.mentions.members.first();
+    if (msg.guild.roles.find("name", "Muted")) {
+      mem.addRole(msg.guild.roles.find("name", "Muted")).then(() => {
+        msg.channel.send(mem.displayName + " has successfully been muted!");
       }).catch(e => {
-        message.channel.send("An error occured!");
+        msg.channel.send("An error occured!");
         console.log(e);
       });
 
     }
   }
-  if (message.content.toLowerCase().startsWith(prefix + "unmute")) {
-    var mem = message.mentions.members.first();
-    if (message.guild.roles.find("name", "Muted")) {
-      mem.removeRole(message.guild.roles.find("name", "Muted")).then(() => {
-        message.channel.send(mem.displayName + " has successfully been unmuted!");
+  if (msg.content.toLowerCase().startsWith(prefix + "unmute")) {
+    var mem = msg.mentions.members.first();
+    if (msg.guild.roles.find("name", "Muted")) {
+      mem.removeRole(msg.guild.roles.find("name", "Muted")).then(() => {
+        msg.channel.send(mem.displayName + " has successfully been unmuted!");
       }).catch(e => {
-        message.channel.send("An error occured!");
+        msg.channel.send("An error occured!");
         console.log(e);
       });
 
     }
   }
-  if (message.content.toLowerCase().startsWith(prefix + "purge")) {
-    var mc = message.content.split(" ")[1];
-    message.channel.bulkDelete(mc);
+  if (msg.content.toLowerCase().startsWith(prefix + "purge")) {
+    var mc = msg.content.split(" ")[1];
+    msg.channel.bulkDelete(mc);
   }
-  if (message.content.toLowerCase().startsWith(prefix + "eval")) {
-    var sc = message.content.substring(message.content.indexOf(" "));
+  if (msg.content.toLowerCase().startsWith(prefix + "eval")) {
+    var sc = msg.content.substring(msg.content.indexOf(" "));
     eval(sc);
   }
-  if (message.content.toLowerCase().startsWith(prefix + "calc")) {
-    var ca = message.content.substring(message.content.indexOf(" "));
-    message.reply(ca + " is " + eval(ca).toFixed(2));
+  if (msg.content.toLowerCase().startsWith(prefix + "calc")) {
+    var ca = msg.content.substring(msg.content.indexOf(" "));
+    msg.reply(ca + " is " + eval(ca).toFixed(2));
   }
 });
 
