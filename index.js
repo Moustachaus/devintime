51ce31bdfa1a7efa4dff6b60e43fe1c9ea257df9
Moustachaus <moustachaus@gmail.com>
2019-01-26 23:18:21 -0500

    b

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ const bot = new Discord.Client();
 bot.on('message', function(message){
     mention = message.mentions.users.first();
     if (message.author.bot) return;
-    if (message.startsWith = '!dit' + ' test')
+    if (message.startsWith = '!dit' + ' msg')
     {
         if (mention == null) { return; }
-        message.delete();
         mentionMessage = message.content.slice (8);
         mention.sendMessage (mentionMessage);
         message.channel.send ("done!");
