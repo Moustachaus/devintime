284cb167eda9572101c8a5580cb9685ab9a376ad
Moustachaus <moustachaus@gmail.com>
2019-01-27 12:22:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,16 +8,14 @@ const bot = new Discord.Client();
 bot.on('message', function(message){
     mention = message.mentions.users.first();
     if (message.author.bot) return;
-    if (message.startsWith = '!devintime msg')
+    if (message.content = '!devintime msg')
     {
         if (mention == null) { return; }
         if (message.member.hasPermission("ADMINISTRATOR"))
         {
-            if (message.startsWith = '!devintime msg') {
-                mentionMessage = message.content.slice (15);
-                mention.sendMessage (mentionMessage);
-                message.channel.send ("Envoyé!");
-            }
+            mentionMessage = message.content.slice (15);
+            mention.sendMessage (mentionMessage);
+            message.channel.send ("Envoyé!");
         } else {
             message.channel.send ("Tu n'as pas la permission de faire ça !");
           }
