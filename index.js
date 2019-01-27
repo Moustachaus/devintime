5ea868780e822bef966dd66baff3cc47cd8326d9
Moustachaus <moustachaus@gmail.com>
2019-01-27 14:42:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ const prefix = '!devintime';
 
 
 
-bot.on('message', function(message){
+bot.on('message', message => {
 
 
     if (message.author.bot) return;
 
     mention = message.mentions.users.first();
 
-    if (message.content === prefix + ' msg') {
-        if (mention == null) { return; }
-        if (message.member.hasPermission("ADMINISTRATOR"))
-        {
-            mentionMessage = message.content.slice (15);
-            mention.sendMessage (mentionMessage);
-            message.channel.send ("Envoyé!");
-        } else {
-            message.channel.send ("Tu n'as pas la permission de faire ça !");
-          }
+    if (message.content[0] === prefix) {
+        if(message.content === 'test') {
+            if (mention == null) { return; }
+            if (message.member.hasPermission("ADMINISTRATOR"))
+            {
+                mentionMessage = message.content.slice (15);
+                mention.sendMessage (mentionMessage);
+                message.channel.send ("Envoyé!");
+            } else {
+                message.channel.send ("Tu n'as pas la permission de faire ça !");
+              }
+        }
         
-    } else {
-        return;
     }
     
 });
