ec818e1a13d54463a5d304f37664a22189c28aed
Moustachaus <moustachaus@gmail.com>
2019-01-27 15:39:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ bot.on('message', message => {
                 if(splitMessage.length === 4)
                 if (mention == null) { return sendError(message, 'Erreur avec la mention') }
                 if (message.member.hasPermission("ADMINISTRATOR"))  {
-                    mentionMessage = splitMessage[4];
+                    mentionMessage = (splitMessage[4]);
                     mention.sendMessage (mentionMessage);
                     message.channel.send ("Envoyé!");
                 } else {
