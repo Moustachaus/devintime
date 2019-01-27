1d76e40720062d8bf2a4691a33ba173c5a6c2840
Moustachaus <moustachaus@gmail.com>
2019-01-26 21:49:59 -0500

    n

--- index.js
+++ index.js
@@ -2,3 +2,25 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 bot.login(process.env.toek);
+
+
+client.msgs = require ("./msgs.json");
+
+
+
+const prefix = "!dit";
+client.on ("message", (message) => {
+
+    msg = message.content.toLowerCase();
+
+    if (message.author.bot) return;
+
+    mention = message.mentions.users.first();
+
+    if (msg.startsWith (prefix + "send")) {
+        if (mention == null) { return; }
+        message.delete();
+        mentionMessage = message.content.slice (8);
+        metnion.sendMessage (mentionMessage);
+        message.channel.send ("done!"); 
+    }
