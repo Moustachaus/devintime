90ada3f60e8f3a010ad831a395807a890bf9ebe7
Moustachaus <moustachaus@gmail.com>
2019-01-28 20:20:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -76,6 +76,27 @@ bot.on('message', message => {
                             .addField("Temps de votre absence", splitMessage[3], true)
                             .setColor("0xFACC2E")
                             .setFooter("Dev in Time")
+
+
+                            const filter = (reaction, user) => {
+                                return ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id;
+                            };
+                            
+                            message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+                                .then(collected => {
+                                    const reaction = collected.first();
+                            
+                                    if (reaction.emoji.name === '👍') {
+                                        message.reply('👍');
+                                    }
+                                    if (reaction.emoji.name === '👎') {
+                                        message.reply('👎');
+                                    }
+                                })
+                                .catch(collected => {
+                                    console.log(`After a minute, only ${collected.size} out of 4 reacted.`);
+                                    message.reply('you didn\'t react with neither a thumbs up, nor a thumbs down.');
+                                });
                         message.channel.send({embed: embed1}).then(embedMessage => {
                             embedMessage.react('👍').then(() => embedMessage.react('👎'));
                         });
