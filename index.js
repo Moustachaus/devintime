3e83d23e36a2508902ae56a084e8b4f1f7f648b1
Moustachaus <moustachaus@gmail.com>
2019-01-29 16:33:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -267,17 +267,17 @@ bot.on('message', message => {
         }
         if(splitMessage[1] === 'admin') {
             if(splitMessage[2] === 'aide') {
-                 if(splitMessage.length === 3) {
-                   if(message.member.roles.has("535827594432610336")) {
-                    var adminhelp = new Discord.RichEmbed()
-                    .setTitle("Aide Admin")
-                    .setDescription(" ")
-                    .addField("Commandes", "!devintime absent (raison) (temps)", "Pour nous prévenir d'une absence de votre part", true)
-                    .addField("2", "test", true)
-                    .setColor("0xFACC2E")
-                    .setFooter("Dev in Time")
+                if(splitMessage.length === 3) {
+                    if(message.member.roles.has("535827594432610336")) {
+                        var adminhelp = new Discord.RichEmbed()
+                        .setTitle("Aide Admin")
+                        .setDescription(" ")
+                        .addField("Commandes", "!devintime absent (raison) (temps)", "Pour nous prévenir d'une absence de votre part", true)
+                        .addField("2", "test", true)
+                        .setColor("0xFACC2E")
+                        .setFooter("Dev in Time")
                     
-                    message.channel.send({embed: adminhelp})
+                        message.channel.send({embed: adminhelp})
 
 
                 }
