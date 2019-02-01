c9d6e727fb023694b8484ffdf6adc9515dd020fe
Moustachaus <moustachaus@gmail.com>
2019-02-01 17:25:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -223,6 +223,7 @@ bot.on('guildMemberAdd', member => {
     .addField("**Membre: **", member.guild.memberCount, false)
     .setColor("0x53DD73")
     .setFooter("• Bienvenue sur Dev in Time •", member.user.displayAvatarURL)
+    .setTimestamp
 
     member.sendEmbed(msgbvnembed);
     member.guild.channels.get('535822252717899806').sendEmbed(bvnembed);
@@ -241,6 +242,7 @@ bot.on('guildMemberRemove', member => {
     .addField("**Membre: **", member.guild.memberCount, false)
     .setColor("0xE61919")
     .setFooter("• À la prochaine ! •", member.user.displayAvatarURL)
+    .setTimestamp
 
     member.guild.channels.get('535822252717899806').sendEmbed(byembed);
 
