a5775e9946fb68972fb6ee01adba2f3ba98693d6
Moustachaus <moustachaus@gmail.com>
2019-02-02 16:29:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ bot.on('message', message => {
                         .setTitle("Envoyé!")
                         .setDescription("Absence reçus")
                         .addField("Raison de l'absence:", tMessage, true)
-                        .addField("De:", user.username, true)
+                        .addField("De:", message.member.user.tag, true)
                         .setColor("0xFACC2E")
                         .setFooter("• Dev in Time •")
 
