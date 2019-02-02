670cb185a9621ed29d02fd823c3d6e4009ca6f44
Moustachaus <moustachaus@gmail.com>
2019-02-01 20:04:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -221,7 +221,7 @@ bot.on('message', message => {
     }
         else if(splitMessage[0] === '!devintime') {
             if(splitMessage[1] === 'commande') {
-                if (msg.channel.id === '539120346624819210') {
+                if (message.channel.id === '539120346624819210') {
             //var commande = new Discord.RichEmbed()
             //    .setTitle("Commande personnalisée envoyé !")
             //    .setDescription("**Votre commande personnalisée à été envoyé !**")
